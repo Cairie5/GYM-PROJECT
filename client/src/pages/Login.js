d40aabe754b281880040442567c72832fcd37d27
1Markish <mark.kipchumba@student.moringaschool.com>
2023-10-16 23:37:27 +0300

    Requirements updated

---
+++ client/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Signin from '../components/Signin';
+
+function Register(){
+
+    return (
+        <div>
+            <Signin />
+        </div>
+    )
+}
+
+
+export default Register
